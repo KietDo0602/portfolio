c01201d324a5c795f2e734b9bb15c23d48182db3
Mark Watney <kietdo0602@gmail.com>
2022-09-10 22:02:54 -0400

    Attempt fix routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect, useRef} from 'react';
 import Home from './Home';
 import NotFoundPage from './NotFoundPage';
 import ResumePage from './ResumePage';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Projects from './Projects';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/all-projects" element={<Projects/>} title="Projects"/>
           <Route exact path="/files/resume.pdf" element={<ResumePage />} title="Resume"/>
           <Route path="/404" element={<NotFoundPage />} title="Error"/>
-          <Route path="*" element={<NotFoundPage />} title="Error"/>
+          <Route path="*" element={<Navigate to="/404" replace />} />
         </Routes>
     </Router>
     </div>
